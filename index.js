db6838d495b1ac3936bcebdb7e811ed8d2d9bee9
dekc89 <dekc89@gmail.com>
2018-11-14 21:11:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
 const inc = (a) => a+=1;
-const dec = (thisIsJustAPlaceholderDontMindMe) => a-+1;
+const dec = (thisIsJustAPlaceholderDontMindMe) => a-=1;
